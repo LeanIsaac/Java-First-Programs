573a545fdf7e06f1fff03bdb5227ffa7f04631c7
LeanIsaac <leancarp09@gmail.com>
2023-03-14 17:56:28 -0300

    finalizacion ByteBank

--- Proyecto-ByteBank/src/PruebaConstructores.java
+++ Proyecto-ByteBank/src/PruebaConstructores.java
@@ -1,9 +1,12 @@
 
 public class PruebaConstructores {
 	public static void main(String[] args) {
-		Cuenta cuenta = new Cuenta();
-		cuenta.setAgencia(-2);
+		Cuenta cuenta = new Cuenta();//INSTANCIA 1
+		Cuenta cuenta2 = new Cuenta();//INSTANCIA 2
 		
-		System.out.println(cuenta.getAgencia());
+		//cuenta.setAgencia(2);
+		
+		//System.out.println(cuenta.getAgencia());
+		System.out.println(Cuenta.getTotal());
 	}
 }
